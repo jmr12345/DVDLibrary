9182df62bd5cc124cceaa0d692f316bf67edf666
Amy Chiu <amyechiu@gmail.com>
2014-03-04 22:41:23 -0600

    added splash screen

---
+++ DVDLibrary/Controllers/SplashScreenViewController.m
@@ -0,0 +1,29 @@
+//
+//  SplashScreenViewController.m
+//  DVDLibrary
+//
+//  Created by Amy Chiu on 3/4/14.
+//  Copyright (c) 2014 Ming. All rights reserved.
+//
+
+#import "SplashScreenViewController.h"
+
+@interface SplashScreenViewController ()
+
+@end
+
+@implementation SplashScreenViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
